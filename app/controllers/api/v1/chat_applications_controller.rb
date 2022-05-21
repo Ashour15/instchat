da3717e1d7dd2530e619304a3940e92c91897329
Ashour15 <mohamedashour15@hotmail.com>
2022-05-21 04:00:17 +0200

    Init controllers, routes and create all actions for chat applications controller

---
+++ app/controllers/api/v1/chat_applications_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::ChatApplicationsController < ApplicationController
+  before_action :set_chat_application, only: [:show, :update, :destroy]
+
+  #GET /api/v1/chat_applications/:token
+  def index 
+    chat_applications = ChatApplication.all
+    render json: chat_applications, status: :ok
+  end
+  
+  #GET /api/v1/chat_applications/:token
+  def show 
+    render json: @chat_application, status: :ok
+  end
+
+  #POST /api/v1/chat_applications
+  def create
+    chat_application = ChatApplication.new(chat_application_params)
+    if chat_application.save
+      render json: chat_application, status: :created
+    else
+      render json: chat_application.errors, status: :unprocessable_entity
+    end
+  end
+
+  #PUT /api/v1/chat_applications/:token
+  def update
+    if @chat_application.update(chat_application_params)
+      render json: @chat_application, status: :ok
+    else
+      render json: @chat_application.errors, status: :unprocessable_entity
+    end
+  end
+
+  #DELETE /api/v1/chat_applications/:token
+  def destroy
+    @chat_application.destroy
+    head :no_content
+  end
+
+  private
+
+  def chat_application_params
+    params.require(:chat_application).permit(:name)
+  end
+
+  def set_chat_application
+    @chat_application = ChatApplication.find_by_token(params[:token])
+  end
+end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def as_json(options = nil)
+    super(except: :id)
+  end
 end
