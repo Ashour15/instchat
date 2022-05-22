3894597414084f1301a39118371a2ed5c0b2abe8
Ashour15 <mohamedashour15@hotmail.com>
2022-05-22 06:00:53 +0200

    Remove not needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :chat_applications, param: :token do
-        resources :chats, param: :number do
-          resources :messages, param: :number
+      resources :chat_applications,  param: :token do
+        resources :chats, only: [:create, :index, :show], param: :number do
+          resources :messages, only: [:create, :index, :show], param: :number
         end
       end
     end
